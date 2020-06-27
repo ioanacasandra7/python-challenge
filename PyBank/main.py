a3faf1d4e7e28c8964929962c3551c6daabc2c3d
ioanacasandra7 <ioanacasandraH@gmail.com>
2020-06-27 09:27:37 -0500

    created file PyBank and PyPoll

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,17 +1,52 @@
 import os
 import csv
 
+#Open and store the content of budget_data.csv in the variable csvreader
 csvpath=os.path.join('/Users/ioanahancu/python-challenge/PyBank/Resources/budget_data.csv')
-with open(csvpath, newline='') as csvfile:
+
+total_months = []
+total_profit = []
+monthly_profit_change = []
+
+with open(csvpath)as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     print(csvreader)
-    csv_header = next(csvreader)
-    month = []
-    revenue = []
-    revenue_change = []
-    monthly_change = []
-    
-    print(f"Header: {csv_header}") 
+    csv_header =next(csvreader)
+    print(f"Header: {csv_header}")  
+
+for row in csvreader:
+    print(row[0])
+
+#Append month and profit/loss to their corresponding lists
+#for row in csvreader:
+    #total_months.append(row[0])
+    #total_profit.append(row[1])
+
+# Take the difference between two months and append to monthly profit change
+#for i in range(1, len(total_profit)):
+    #monthly_profit_change.append(total_profit[i+1]-total_profit[i])
+
+# Obtain the max and min of the the montly profit change list
+#max_increase_value = max(monthly_profit_change)
+#max_decrease_value = min(monthly_profit_change)
+
+#We use the plus 1 at the end because the month associated with change is the next month
+#max_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
+#max_decrease_month = monthly_profit_change.index(min(monthly_profit_change)) + 1 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,5 +1,51 @@
 import os
 import csv
 
-csvpath=os.path.join("/Users/ioanahancu/python-challenge/PyPoll/Resources/election_data.csv')
+#Store the contents of election_data.csv in the variable csvpath
+csvpath=os.path.join('/Users/ioanahancu/python-challenge/PyPoll/Resources/election_data.csv')
+with open(csvpath)as csvfile:
+  csvreader = csv.reader(csvfile, delimiter=',')
+  print(csvreader)
+  csv_header =next(csvreader)
+
+print(f"Header: {csv_header}")
+
+# Declare Variables 
+total_votes = 0 
+khan_votes = 0
+correy_votes = 0
+li_votes = 0
+otooley_votes = 0
+
+#for row in csvreader:
+  #print(row[0])
+
+#function += retrieved from: https://www.codecademy.com/forum_questions/555729a8d3292f6e7d000655
+#use it to count the total votes
+#total_votes +=1
+#print(total_votes)
+
+#column 2 contains the candidates, so we can loop through it and count the total votes for each candidate
+#if row[2] == "Khan": 
+ ##  khan_votes +=1
+#elif row[2] == "Correy":
+ #  correy_votes +=1
+#elif row[2] == "Li": 
+ #  li_votes +=1
+#elif row[2] == "O'Tooley":
+ #  otooley_votes +=1
+
+#we create a dictionary containing both the candidates and the total number of votes for each candidate:
+#candidates = ["Khan", "Correy", "Li","O'Tooley"]
+#votes = [khan_votes, correy_votes,li_votes,otooley_votes]
+#dict_candidates_and_votes = dict(zip(candidates,votes))
+
+# Get the winner by using the max function of the dictionary 
+#key = max(dict_candidates_and_votes, key=dict_candidates_and_votes.get)
+
+# Print the votes as percentage
+#khan_percent = (khan_votes/total_votes) *100
+#correy_percent = (correy_votes/total_votes) * 100
+#li_percent = (li_votes/total_votes)* 100
+#otooley_percent = (otooley_votes/total_votes) * 100
 
